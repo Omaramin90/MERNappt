2a4dd91f8e45b652e40220b18af9ddfe68072bfe
Omaramin90 <68878625+Omaramin90@users.noreply.github.com>
2021-06-02 11:58:09 -0400

    Add files via upload

---
+++ BE/app.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+const port = 5000
+
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+
+const config = require('./config');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+MongoClient.connect(`mongodb://${config.dbHost}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+    .then(client => {
+      const db = client.db(config.dbName);
+      const collection = db.collection(config.dbCollection);
+      app.locals[config.dbCollection] = collection;
+    })
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-control-allow-headers','Origin, Content-type, accept')
+    res.setHeader('Access-control-allow-methods','GET,POST,PATCH,DELETE')
+
+    next()
+})
+
+app.use((req, res, next) => {
+ const collection = req.app.locals[config.dbCollection];
+ req.collection = collection;
+ next();
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+mongoose.connect('mongodb+srv://omara:testpass@cluster0.efenk.mongodb.net/appointments-app?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then( () => {
+        app.listen(port, () => {
+            console.log("development server start on port-"+ port)
+        })
+    }).catch( () =>{
+        console.log("Unable to connect to db server")
+})
+
+module.exports = app;
